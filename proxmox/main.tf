de8ec830cf222d0c8712d2a449f780cc91b8a1a1
Nigel <you@example.com>
2023-09-04 18:21:52 +0200

    translate tf setup to my homelab

--- proxmox/main.tf
+++ proxmox/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     proxmox = {
       source = "telmate/proxmox"
-      version = "2.9.10"
+      version = "2.9.14"
     }
   }
 }
@@ -23,11 +23,13 @@ provider "proxmox" {
   }
 }
 
-resource "proxmox_vm_qemu" "k8s-master" {
-  name = "k8s-master"
+resource "proxmox_vm_qemu" "talos-controlpane" {
+  name = "talos-cp-1"
   target_node = var.proxmox_target_node
   iso = "local:iso/${var.k8s_iso}"
+  onboot = true
   oncreate = true
+  qemu_os = "other"
   scsihw = "virtio-scsi-pci"
   cpu = "host"
   cores = var.k8s_master_cpu
@@ -36,22 +38,25 @@ resource "proxmox_vm_qemu" "k8s-master" {
   disk {
     type = "scsi"
     storage = "local-lvm"
-    size = "32G"
+    size = "20G"
   }
 
   network {
     model = "virtio"
     bridge = "vmbr0"
     firewall = false
+    macaddr = var.k8s_master_MAC
   }
 }
 
 resource "proxmox_vm_qemu" "k8s-workers" {
   count = var.k8s_worker_count
-  name = "k8s-worker-${count.index}"
+  name = "talos-worker-${count.index}"
   target_node = var.proxmox_target_node
   iso = "local:iso/${var.k8s_iso}"
-  oncreate = false
+  onboot = true
+  oncreate = true
+  qemu_os = "other"
   scsihw = "virtio-scsi-pci"
   cpu = "host"
   cores = var.k8s_worker_cpu
@@ -60,12 +65,13 @@ resource "proxmox_vm_qemu" "k8s-workers" {
   disk {
     type = "scsi"
     storage = "local-lvm"
-    size = "32G"
+    size = "20G"
   }
 
   network {
     model = "virtio"
     bridge = "vmbr0"
     firewall = false
+    macaddr = var.k8s_worker_MAC_addresses[count.index]
   }
 }
--- proxmox/vars.tf
+++ proxmox/vars.tf
@@ -2,7 +2,7 @@
 variable "proxmox_url" {
   type = string
   description = "The API URL of the Proxmox server"
-  default = "https://pve.rcomanne.nl:8006/api2/json"
+  default = "https://10.0.49.20:8006/api2/json"
 }
 
 variable "proxmox_tls_insecure" {
@@ -14,7 +14,7 @@ variable "proxmox_tls_insecure" {
 variable "proxmox_api_key_id" {
   type = string
   description = "The id of the API key used for proxmox authentication"
-  default = "terraform@pve!terraform-01"
+  default = "Terraform@pve!bitbucket-ID"
 }
 
 variable "proxmox_api_key_secret" {
@@ -26,25 +26,31 @@ variable "proxmox_api_key_secret" {
 variable "proxmox_target_node" {
   type = string
   description = "The name of the Proxmox node to create the VMs on."
-  default = "pve"
+  default = "k8s-master-node"
 }
 
 variable "k8s_iso" {
   type = string
   description = "The ISO to use for the k8s nodes"
-  default = "talos-amd64.iso"
+  default = "talos-metal-amd64.iso"
 }
 
 variable "k8s_master_cpu" {
   type = number
   description = "The number of CPU cores to allocate to the master node(s)"
-  default = 2
+  default = 4
 }
 
 variable "k8s_master_ram" {
   type = number
   description = "The amount of RAM to allocate to the master node(s)"
-  default = 2048
+  default = 4096
+}
+
+variable "k8s_master_MAC" {
+  type = string
+  description = "MAC address for master node, for static IP setup"
+  default = "EA:30:79:9C:FF:BE"
 }
 
 variable "k8s_worker_count" {
@@ -56,11 +62,17 @@ variable "k8s_worker_count" {
 variable "k8s_worker_cpu" {
   type = number
   description = "The number of CPU cores to allocate to the master node(s)"
-  default = 3
+  default = 4
 }
 
 variable "k8s_worker_ram" {
   type = number
   description = "The amount of RAM to allocate to the master node(s)"
-  default = 9216
+  default = 4096
+}
+
+variable "k8s_worker_MAC_addresses" {
+  type = list(string)
+  description = "Default MAC addresses to assign static IPs"
+  default = ["2A:B9:CB:63:98:36", "EE:FF:AC:8B:57:0F", "9E:10:27:EB:00:4E"]
 }
