90dff3cfcfeef12f3b7f51b52dc783a4d2b61a97
Nigel <11520032+NigelVanHattum@users.noreply.github.com>
2024-08-28 23:48:01 +0200

    LCM

--- 02_base_resources/_chart_versions.tf
+++ 02_base_resources/_chart_versions.tf
@@ -1,41 +1,47 @@
 variable "linkerd_crd_chart_version" {
   type = string
   description = "linkerd chart version"
-  default = "2024.5.3"
+  default = "2024.8.2"
 }
 
 variable "linkerd_control_plane_chart_version" {
   type = string
   description = "linkerd chart version"
-  default = "2024.5.3"
+  default = "2024.8.2"
 }
 
 variable "argocd_chart_version" {
   type    = string
   description = "argocd chart version"
-  default = "6.9.3"
+  default = "7.4.1"
+}
+
+variable "k8s_monitoring_chart_version" {
+  type    = string
+  description = "grafana monitoring"
+  default = "1.8.8"
 }
 
 variable "nfs_csi_driver_chart_version" {
   type    = string
-  default = "v4.7.0"
+  default = "v4.8.0"
 }
 
 variable "metallb_chart_version" {
   type = string
   description = "MetalLB version"
-  default = "0.14.5"
+  default = "0.14.8"
 }
 
 variable "traefik_chart_version" {
   type = string
   description = "Traefik chart version (not traefik version)"
-  default = "28.0.0"
+  default = "30.0.2"
 }
 
 variable "cnpg_postgres_operator_chart_version" {
   type    = string
-  default = "0.21.5"
+  default = "0.21.6"
 }
 
 variable "cnpg_postgres_cluster_chart_version" {
@@ -51,5 +57,5 @@ variable "influxdb_chart_version" {
 variable "authentik_chart_version" {
   type = string
   description = "authentik chart version"
-  default = "2024.6.1"
+  default = "2024.6.3"
 }
