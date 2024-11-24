c81e7c759658b5926414086f1128d71c79af84ba
Nigel <11520032+NigelVanHattum@users.noreply.github.com>
2024-11-24 10:05:20 +0100

    LCM

--- 02_base_resources/_chart_versions.tf
+++ 02_base_resources/_chart_versions.tf
@@ -1,46 +1,54 @@
-variable "linkerd_chart_version" {
-  type = string
-  description = "linkerd chart version"
-  default = "2024.10.2"
-}
+# https://github.com/linkerd/linkerd2/tree/main/charts/linkerd-control-plane
+# variable "linkerd_chart_version" {
+#   type = string
+#   description = "linkerd chart version"
+#   default = "2024.11.3"
+# }
 
+# https://github.com/argoproj/argo-helm
 variable "argocd_chart_version" {
   type    = string
   description = "argocd chart version"
-  default = "7.5.2"
+  default = "7.7.5"
 }
 
+# https://github.com/grafana/k8s-monitoring-helm
 variable "k8s_monitoring_chart_version" {
   type    = string
   description = "grafana monitoring"
-  default = "1.5.1"
+  default = "1.6.11"
 }
 
+# https://github.com/kubernetes-csi/csi-driver-nfs
 variable "nfs_csi_driver_chart_version" {
   type    = string
   default = "v4.9.0"
 }
 
+# https://github.com/metallb/metallb/tree/main/charts/metallb/charts/crds
 variable "metallb_chart_version" {
   type = string
   description = "MetalLB version"
   default = "0.14.8"
 }
 
+# https://github.com/traefik/traefik-helm-chart
 variable "traefik_chart_version" {
   type = string
   description = "Traefik chart version (not traefik version)"
-  default = "31.0.0"
+  default = "33.0.0"
 }
 
+# https://github.com/cloudnative-pg/charts/tree/main/charts/cloudnative-pg
 variable "cnpg_postgres_operator_chart_version" {
   type    = string
-  default = "0.22.0"
+  default = "0.22.1"
 }
 
+# https://github.com/cloudnative-pg/charts/tree/main/charts/cluster
 variable "cnpg_postgres_cluster_chart_version" {
   type    = string
-  default = "0.0.11"
+  default = "0.1.0"
 }
 
 variable "influxdb_chart_version" {
@@ -48,8 +56,10 @@ variable "influxdb_chart_version" {
   default = "2.1.2"
 }
 
+# https://github.com/goauthentik/helm
+# https://docs.goauthentik.io/docs/releases
 variable "authentik_chart_version" {
   type = string
   description = "authentik chart version"
-  default = "2024.8.1"
+  default = "2024.10.4"
 }
