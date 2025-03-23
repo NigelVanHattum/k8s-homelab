148b16c580d62b13ddcf567f254a94442c52677c
Hattum van <Nigel.van.Hattum@rabobank.nl>
2025-03-24 00:14:51 +0100

    LCM onepassword provider

--- 03_applications/_versions.tf
+++ 03_applications/_versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     onepassword = {
       source = "1Password/onepassword"
-      version = ">= 1.4.1, < 2.0.0"
+      version = ">= 2.0.0, < 3.0.0"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
