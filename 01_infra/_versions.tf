c338ea0b6d9f130929f94ce5cbc20a79f18d42ad
Nigel <11520032+NigelVanHattum@users.noreply.github.com>
2025-05-04 22:35:43 +0200

    Adding quick links to releases

--- 01_infra/_versions.tf
+++ 01_infra/_versions.tf
@@ -1,18 +1,22 @@
 terraform {
+  # https://github.com/hashicorp/terraform/releases
   required_version = ">= 1.8.0, < 2.0.0"
   required_providers {
     onepassword = {
       source = "1Password/onepassword"
       version = ">= 2.0.0, < 3.0.0"
     }
+    # https://registry.terraform.io/providers/Telmate/proxmox/latest/docs
     proxmox = {
       source = "telmate/proxmox"
-      version = "3.0.1-rc6"
+      version = "3.0.1-rc8"
     }
+    # https://registry.terraform.io/providers/siderolabs/talos/0.8.0
     talos = {
       source = "siderolabs/talos"
-      version = ">= 0.7.0, < 0.8.0"
+      version = ">= 0.8.0, < 0.9.0"
     }
+    # https://registry.terraform.io/providers/alekc/kubectl/latest
     kubectl = {
       source = "alekc/kubectl"
       version = ">= 2.0.0, < 3.0.0"
