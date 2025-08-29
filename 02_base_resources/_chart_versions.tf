3f372757c6622aa093a17c8324ac1952249ab6aa
Nigel <11520032+NigelVanHattum@users.noreply.github.com>
2025-08-29 11:29:04 +0200

    Missed some updates during downtime

--- 02_base_resources/_chart_versions.tf
+++ 02_base_resources/_chart_versions.tf
@@ -9,14 +9,14 @@
 variable "argocd_chart_version" {
   type    = string
   description = "argocd chart version"
-  default = "8.2.5"
+  default = "8.3.1"
 }
 
 # https://github.com/grafana/k8s-monitoring-helm
 variable "k8s_monitoring_chart_version" {
   type    = string
   description = "grafana monitoring"
-  default = "3.2.3"
+  default = "3.3.2"
 }
 
 # https://github.com/kubernetes-csi/csi-driver-nfs
@@ -48,7 +48,7 @@ variable "traefik_chart_version" {
 # https://github.com/cloudnative-pg/charts/tree/main/charts/cloudnative-pg
 variable "cnpg_postgres_operator_chart_version" {
   type    = string
-  default = "0.25.0"
+  default = "0.26.0"
 }
 
 # https://github.com/cloudnative-pg/charts/tree/main/charts/cluster
@@ -57,15 +57,10 @@ variable "cnpg_postgres_cluster_chart_version" {
   default = "0.3.1"
 }
 
-variable "influxdb_chart_version" {
-  type    = string
-  default = "2.1.2"
-}
-
 # https://github.com/rm3l/helm-charts/blob/main/charts/adguard-home/README.md
 variable "adguard_home_chart_version" {
   type    = string
-  default = "0.20.0"
+  default = "0.20.1"
 }
 
 # https://github.com/goauthentik/helm
@@ -73,5 +68,5 @@ variable "adguard_home_chart_version" {
 variable "authentik_chart_version" {
   type = string
   description = "authentik chart version"
-  default = "2025.6.4"
+  default = "2025.8.1"
 }
