a2f26920ab300bfd03f7028232215aa0571b2639
Nigel <11520032+NigelVanHattum@users.noreply.github.com>
2025-12-22 14:32:35 +0100

    Update hashicorp/kubernetes provider version constraint to allow 3.x versions

--- 03_applications/_versions.tf
+++ 03_applications/_versions.tf
@@ -7,7 +7,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.23.0, < 3.0.0"
+      version = ">= 2.23.0, < 4.0.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
